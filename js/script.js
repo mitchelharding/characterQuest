a593cc6f3e73b0d7d8ca0e3b9253ddbd35264a6a
Mitchel Harding <hardingmitchel@gmail.com>
2017-11-09 20:28:33 -0700

    DAMMIT

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 $(document).ready(function(){
   $("form#form-group").submit(function(){
 
-    var a1 = $("#leader").val();
-    var a2 = $("#follower").val();
-    var b1 = $("#smart").val();
-    var b2 = $("#dumb").val();
-    var c1 = $("#strong").val();
-    var c2 = $("#weak").val();
-    var d1 = $("#rebel").val();
-    var d2 = $("#conform").val();
-    var e1 = $("#amb").val();
-    var e2 = $("#lazy").val();
-    var f1 = $("#book").val();
-    var f2 = $("#movie").val();
+    var a1 = parseString($("#leader").val());
+    var a2 = parseString($("#follower").val());
+    var b1 = parseString($("#smart").val());
+    var b2 = parseString($("#dumb").val());
+    var c1 = parseString($("#strong").val());
+    var c2 = parseString($("#weak").val());
+    var d1 = parseString($("#rebel").val());
+    var d2 = parseString($("#conform").val());
+    var e1 = parseString($("#amb").val());
+    var e2 = parseString($("#lazy").val());
+    var f1 = parseString($("#book").val());
+    var f2 = parseString($("#movie").val());
 
-    var dwights = a1 + b1 + c1 + d2 + e1 + f2;
-    var jimh = a1 + b1 + c1 + d1 + e2 + f2;
-    var pamh = a2 + b2 + c1 + d2 + e2 + f1;
+    var dwights = a1 === "Leader" || b1 === "Leader" || c1 === "Strong Willed"|| d2 === "Confirmist" || e1 === "Ambitous" || f2 === "Movie";
+
 
 
     if (dwights) {
