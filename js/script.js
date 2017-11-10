01e845181da2c4e10a987128b530b49dd0cca022
Mitchel Harding <hardingmitchel@gmail.com>
2017-11-10 10:55:57 -0700

    Still messing with different variable options.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,31 @@
 $(document).ready(function(){
   $("form#form-group").submit(function(){
 
-    var a1 = parseString($("#leader").val());
-    var a2 = parseString($("#follower").val());
-    var b1 = parseString($("#smart").val());
-    var b2 = parseString($("#dumb").val());
-    var c1 = parseString($("#strong").val());
-    var c2 = parseString($("#weak").val());
-    var d1 = parseString($("#rebel").val());
-    var d2 = parseString($("#conform").val());
-    var e1 = parseString($("#amb").val());
-    var e2 = parseString($("#lazy").val());
-    var f1 = parseString($("#book").val());
-    var f2 = parseString($("#movie").val());
+    var result = forEach.function((userSelect){
 
-    var dwights = a1 === "Leader" || b1 === "Leader" || c1 === "Strong Willed"|| d2 === "Confirmist" || e1 === "Ambitous" || f2 === "Movie";
+    var leader1 = parseInt($("option#leader").val());
+    var follower1 = parseInt($("option#follower").val());
+    var smart1 = parseInt($("option#smart").val());
+    var dumb1 = parseInt($("option#dumb").val());
+    var strong1 = parseInt($("option#strong").val());
+    var weak1 = parseInt($("option#weak").val());
+    var rebel1 = parseInt($("option#rebel").val());
+    var conform1 = parseInt($("option#conform").val());
+    var ambitious1 = parseInt($("option#ambitious").val());
+    var lazy1 = parseInt($("option#lazy").val());
+    var book1 = parseInt($("option#book").val());
+    var movie1 = parseInt($("option#movie").val());
 
+    var result = forEach.(userInputs);
 
+    var dwights = 1 + 3 + 5 + 8 + 9 + 12;
 
     if (dwights) {
       $("#result").text("Dwight Shrute");
-      $(".result").show();
-      $("#dwights").show();
-    } else if (jimh) {
-      $("#result").text("Jim Helbert");
-      $(".result").show();
-      $("#jimh").show();
-    } else if (pamh) {
-      $("#result").text("Pam Helbert");
-      $(".result").show();
-      $("#pamh").show();
-    }
+      alert
 
     event.preventDefault();
 
+    });
   });
 });
