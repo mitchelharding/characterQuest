51f1723a2673882c82329fab8ce0a7c60b653b4a
Mitchel Harding <hardingmitchel@gmail.com>
2017-11-11 09:21:36 -0700

    Modify script without succes, need to find a way to log items in a list

--- js/script.js
+++ js/script.js
@@ -1,31 +1,18 @@
 $(document).ready(function(){
-  $("form#form-group").submit(function(){
 
-    var result = forEach.function((userSelect){
+  $("#form-group").submit(function(event){
 
-    var leader1 = parseInt($("option#leader").val());
-    var follower1 = parseInt($("option#follower").val());
-    var smart1 = parseInt($("option#smart").val());
-    var dumb1 = parseInt($("option#dumb").val());
-    var strong1 = parseInt($("option#strong").val());
-    var weak1 = parseInt($("option#weak").val());
-    var rebel1 = parseInt($("option#rebel").val());
-    var conform1 = parseInt($("option#conform").val());
-    var ambitious1 = parseInt($("option#ambitious").val());
-    var lazy1 = parseInt($("option#lazy").val());
-    var book1 = parseInt($("option#book").val());
-    var movie1 = parseInt($("option#movie").val());
+    var formgroups = ["leader", "follower", "smart", "dumb", "strong", "weak", "rebel", "conform", "ambititous", "lazy", "book", "movie"];
 
-    var result = forEach.(userInputs);
+    formgroups.map(function(formgroup){
+      var userSelect = console.log($("option#" + formgroup).val());
+    });
 
-    var dwights = 1 + 3 + 5 + 8 + 9 + 12;
+    if (userSelect[0] === "leader" ){
+    alert("hey");
+    }
 
-    if (dwights) {
-      $("#result").text("Dwight Shrute");
-      alert
 
     event.preventDefault();
-
-    });
   });
 });
