bcdb888d6a230329fdfe42e29ba60530720e42fc
Mitchel Harding <hardingmitchel@gmail.com>
2017-11-15 11:59:51 -0700

    return input values and sent then to an array with specified value but could bot match array with ifThen statement

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
-
     $("form#form-group").submit(function(event){
-
-
-      var results =[""];
-
-      results.map(function(result){
-      var chosen = $("select option: checked" + result).val();
-      $("." + result).text();
-
-      )};
-      alert(chosen);
       event.preventDefault();
 
+       var userInput = [];
+
+        $("input:checkbox[name=characterQ]:checked").each(function(){
+          var result = $(this).val();
+          userInput.push(result);
+        });
+
+           if (userInput = "leader", "smart"){
+             $("#result").show();
+             $(".result").text("Dwight Shrute");
+             $("img#dwights").show();
+            }
+
+
+         $("form#form-group").hide();
     });
   });
